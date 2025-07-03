9c61f7e5fd1eeea54415c3c40951ce8f61c7189f
ArjanDeo <arjandeo@bluecomet-it.com>
2025-07-03 22:42:50 +0100

    Added option to use outside of GTA music

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ settings_file = 'settings.json'
 def check_settings_file():
     if not os.path.exists(settings_file):
         print("Settings file not found.")
-        user_music_folder = input("Please enter the path to your GTA user music folder: ")
+        user_music_folder = input("Please enter the path to your GTA user music folder (If not using GTA, enter path to wanted destination folder): ")
         
-        if not os.path.isdir(user_music_folder):
+        if user_music_folder == "":
+            print('No GTA User Music / Destination folder selected. Skipping.')
+        elif not os.path.isdir(user_music_folder):
             print("The provided path is not valid. Please check the folder path.")
             return None
         
@@ -25,16 +27,24 @@ def check_settings_file():
         else:
             quality = 'm4a/bestaudio/best'
 
-        settings = {"gta_music_folder": user_music_folder, "audio_quality": quality}
+        gta_mode = input("(1) Tag audio files for GTA\n(2) Tag audio files normally\n(Default is 1)")
+        if gta_mode == '1':
+            gta = True
+        elif gta_mode == '2':
+            gta = False
+        else:
+            gta = True
+
+        settings = {"gta_music_folder": user_music_folder, "audio_quality": quality, 'gta_mode': gta}
         with open(settings_file, 'w') as f:
             json.dump(settings, f, indent=4)
         
-        print(f"Settings file created with GTA music folder: {user_music_folder} and audio quality: {quality}")
+        print(f"Settings file created with GTA music folder: {user_music_folder}, audio quality: {quality} and {gta} for GTA mode.")
         return user_music_folder, quality
     else:
         with open(settings_file, 'r') as f:
             settings = json.load(f)
-        return settings.get("gta_music_folder"), settings.get("audio_quality")
+        return settings.get("gta_music_folder"), settings.get("audio_quality"), settings.get("gta_mode")
 
 def install_ffmpeg():
     try:
@@ -52,7 +62,7 @@ def searchForSong(song_name, artist_name):
 def main():
     install_ffmpeg()
     
-    music_folder, audio_quality = check_settings_file()
+    music_folder, audio_quality, gta_mode = check_settings_file()
     if not music_folder:
         return
     while True:
@@ -95,7 +105,10 @@ def main():
 
         audio = MP4(file_path)
         audio["\xa9nam"] = results[choice]['title']
-        audio["\xa9ART"] = artists.upper()
+        if (gta_mode):
+            audio["\xa9ART"] = artists.upper()
+        else:
+            audio["\xa9ART"] = artists
         audio.save()
         print('Successfully changed audio metadata.')
 
